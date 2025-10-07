b076db63e9a8c2452036dbaaa0c5b5822ea4e64f
lumenry <lumenryyt@gmail.com>
2025-10-07 21:18:56 +0200

    Initial commit

---
+++ src/main/java/net/lumenry/upsidedown/UpsidedownDataGenerator.java
@@ -0,0 +1,11 @@
+package net.lumenry.upsidedown;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class UpsidedownDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
