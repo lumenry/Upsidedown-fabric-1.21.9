6e278a7189164c18c68b32ca356d2d4873a690e2
lumenry <lumenryyt@gmail.com>
2025-10-08 18:25:28 +0200

    Changed Mappings from Yarn to ParchementMC and created a simple test in ExampleMixin which prints Hello World to the console when the player swims.

--- src/main/java/net/lumenry/upsidedown/mixin/ExampleMixin.java
+++ src/main/java/net/lumenry/upsidedown/mixin/ExampleMixin.java
@@ -1,15 +1,23 @@
 package net.lumenry.upsidedown.mixin;
 
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MinecraftServer.class)
-public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadWorld")
+
+@Mixin(Player.class)
+public abstract class ExampleMixin {
+    @Shadow
+    public abstract boolean isSwimming();
+
+    @Inject(at = @At("HEAD"), method = "tick")
 	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadWorld()V
+		// This code is injected into the start of MinecraftServer.loadWorld()
+        if(this.isSwimming()){
+            System.out.println("Hello World");
+        }
 	}
 }
